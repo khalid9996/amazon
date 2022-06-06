c74254d43e9a26ebc8d858f780a3b84146b3df67
khalid9996 <khankhalid1743@gmail.com>
2022-06-06 20:04:47 +0530

    second commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import './home.css'
+import Product from './Product'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+    <img 
+     className='home__img'
+    src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/SingleTitle/TheBoys3/3000x1200_Hero-Tall_NP._CB636191920_.jpg" 
+    alt="amazon-banner" />
+
+    {/* Product id, title, price, rating, image*/}
+    {/* Product */}
+    <div className="home__row">
+
+      <Product 
+      id="100" 
+      title="iQOO Z5 5G (Mystic Space, 8GB RAM, 128GB Storage) | Snapdragon 778G 5G Processor" 
+      price={399.99} 
+      rating={5} 
+      image="https://m.media-amazon.com/images/I/71wevQRS5qL._SL1196_.jpg"
+      />
+
+      <Product 
+      id="101" 
+      title="Apple iPhone 13 (128GB) - Midnight" 
+      price={899.99} 
+      rating={5} 
+      image="https://m.media-amazon.com/images/I/61VuVU94RnL._SX522_.jpg"
+      />
+    </div>
+
+    <div className="home__row">
+
+      <Product 
+      id="100" 
+      title="SAMSUNG Galaxy S22 Smartphone, Factory Unlocked Android Cell Phone, 256GB, 8K Camera" 
+      price={699.99} 
+      rating={5} 
+      image="https://m.media-amazon.com/images/I/71HUnJvHsbL._SX569_.jpg"
+      />
+
+      <Product 
+      id="100" 
+      title="MSI GeForce RTX 3090 Gaming X Trio 24G I 24GB GDDR6X I 384-bit PCI Express Gen 4 Gaming Graphic Card" 
+      price={1999.99} 
+      rating={5} 
+      image="https://m.media-amazon.com/images/I/81f6GdD799L._SX450_.jpg"
+      />
+
+      <Product 
+      id="100" 
+      title="Intel Core i9-12900K Desktop Processor 16 (8P+8E) Cores up to 5.2 GHz Unlocked LGA1700 600 Series Chipset 125W" 
+      price={899.99} 
+      rating={5} 
+      image="https://m.media-amazon.com/images/I/61ZRFy6GZ4L._SY450_.jpg"
+      />
+    </div>
+
+    </div>
+    
   )
 }
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,62 +1,71 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
-
 import SearchIcon from "@mui/icons-material/Search"
+import {FiShoppingCart} from 'react-icons/fi'
 
 const NavBar = () => {
   return (
     <nav className='navbar'>
 
-        {/* logo left img */}
+        
         <Link to="/">
         <img className='navbar__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
         </Link>
         
 
-        {/* search box */}
+        
         <div className="navbar__search">
         <input type="text" className='navbar__searchinput' />
         <SearchIcon className='navbar__searchicon' />
         </div>
         
 
-        {/* 3 Links */}
-        <div className='tlink'>
+        
+      
         <div className="header__nav">
-          {/* 1st LINK */}
+
+          
           <Link to="/login" className='nav__link'>
             <div className="nav__option">
-            <span className='nav__uptext'>hello khalid</span>
-            <span>Sign In</span>
+            <span className='nav__optionLineOne'>hello khalid</span>
+            <span className='nav__optionLineTwo'>Sign In</span>
             </div>
           </Link>
 
-          {/* 2st LINK */}
+          
           <Link to="/login" className='nav__link'>
             <div className="nav__option">
-            <span className='nav__uptext'>Return</span>
-            <span>Orders</span>
+            <span className='nav__optionLineOne'>Return</span>
+            <span className='nav__optionLineTwo'>Orders</span>
             </div>
           </Link>
 
-          {/* 3st LINK */}
+          
           <Link to="/login" className='nav__link'>
             <div className="nav__option">
-            <span className='nav__uptext'>Your</span>
-            <span>Prime</span>
+            <span className='nav__optionLineOne'>Your</span>
+            <span className='nav__optionLineTwo'>Prime</span>
             </div>
           </Link>
+
           </div>
 
-          {/* 4st LINK */}
           
+          <Link to='/checkout' className='nav__link'>
+            <div className="nav__optionCart">
+              
+              <FiShoppingCart className='cart__icon'/>
+              
+              <span className='nav__optionLineTwo nav__optionCartItem'>0</span>
+            </div>
+          </Link>
 
-        </div>
+        
         
 
 
-        {/* checkout Basket with number */}
+        
 
     </nav>
   )
